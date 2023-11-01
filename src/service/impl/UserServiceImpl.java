fb374e5a62c8a9e2488a788e2ab248f5bafcf95c
behzod.k <b.kambaraliev@itransition.com>
2023-11-01 14:19:12 +0300

    F3 commented removed from method

--- src/service/impl/UserServiceImpl.java
+++ src/service/impl/UserServiceImpl.java
@@ -3,8 +3,10 @@ package service.impl;
 import service.UserService;
 
 public class UserServiceImpl implements UserService {
+    /**
+     * method for saving user
+     * */
     @Override
     public void save(String firstName, String lastName, int age) {
-        //user saved.
     }
 }
