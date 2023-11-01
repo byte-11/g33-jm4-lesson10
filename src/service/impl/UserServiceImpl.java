59c7afae8b59df351c7e7938d282b455b7863746
behzod.k <b.kambaraliev@itransition.com>
2023-11-01 14:07:27 +0300

    F3 user service save method added.

---
+++ src/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package service.impl;
+
+import service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void save(String firstName, String lastName, int age) {
+
+    }
+}
