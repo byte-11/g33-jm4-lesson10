741d9014cac4760c0a90db8846921f8f2ea9674a
behzod.k <b.kambaraliev@itransition.com>
2023-11-01 14:15:07 +0300

    F3 saving locig changed

--- src/service/impl/UserServiceImpl.java
+++ src/service/impl/UserServiceImpl.java
@@ -5,6 +5,6 @@ import service.UserService;
 public class UserServiceImpl implements UserService {
     @Override
     public void save(String firstName, String lastName, int age) {
-
+        //user saved.
     }
 }
